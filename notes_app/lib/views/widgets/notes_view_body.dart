cf7acdc45984a26bf4cde770671a6989a1ec0616
aymen-hacini <aymenhacini2.0@gmail.com>
2023-07-14 15:00:17 +0100

    add floating aciton button

--- notes_app/lib/views/widgets/notes_view_body.dart
+++ notes_app/lib/views/widgets/notes_view_body.dart
@@ -1,11 +1,14 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/notes_view_bar.dart';
 
-import 'custom_note_item.dart';
+
 import 'notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,4 +25,5 @@ class NotesViewBody extends StatelessWidget {
     );
   }
 }
+
 
